185e74d18e265457ba4c44073a33e54db7e49a42
Saif Mahmud Parvez <mahmudalsaif@gmail.com>
2020-07-23 13:36:56 +0600

    Add files via upload

---
+++ 0_1_Knapsack.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   int itemWeight[4] = {2,3,4,5};
+   int itemValue[4] = {3,4,5,6};
+   int bagMaxWeight = 6;
+
+   //implement your code
+   //you may use your own defined function
+   //print out the table filled up with values as I have demonstrated in the class
+
+}
