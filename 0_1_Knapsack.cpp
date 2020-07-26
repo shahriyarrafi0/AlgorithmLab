fd29fb59feea549b7b501c7f3fbb9e4b4d447c64
shahriyarrafi0 <67265632+shahriyarrafi0@users.noreply.github.com>
2020-07-27 00:09:59 +0600

    191-15-2394

--- 0_1_Knapsack.cpp
+++ 0_1_Knapsack.cpp
@@ -1,15 +1,42 @@
 #include<bits/stdc++.h>
-
 using namespace std;
+int knapSack(int W, int wt[], int val[], int n)
+{
+    int i, w;
+    int K[n + 1][W + 1];
+    for (i = 0; i <= n; i++)
+    {
+        for (w = 0; w < W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i-1] <=w)
+            {
+                K[i][w] = max( val[i - 1] + K[i - 1][w - wt[i - 1]],K[i - 1][w]);
+            }
+            else
+            {
+                K[i][w] = K[i - 1][w];
+            }
+            printf("%d    ",K[i][w]);
+        }
+        printf("\n")     ;
 
+    }
+    return K[n][W-1];
+}
+int max(int a, int b)
+{
+(a>b)?a:b;
+}
 int main()
 {
-   int itemWeight[4] = {2,3,4,5};
-   int itemValue[4] = {3,4,5,6};
-   int bagMaxWeight = 6;
+    int weight[] = { 2,3,4,5 };
+    int value[] = { 3,4,5,6 };
+    int MaxWeight = 6;
+    int Size = sizeof(value) / sizeof(value[0]);
+    int highest=knapSack(MaxWeight, weight, value, Size);
+    cout<<"The max possible value  is "<<highest<<endl;
 
-   //implement your code
-   //you may use your own defined function
-   //print out the table filled up with values as I have demonstrated in the class
-
+    return 0;
 }
